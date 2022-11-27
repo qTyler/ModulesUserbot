eb25a14ff79db6dda6f3fc03c34d93f54b549fcf
qTyler <97607757+qTyler@users.noreply.github.com>
2022-11-27 05:57:38 +0300

    Update generate_userlist.py

--- generate_userlist.py
+++ generate_userlist.py
@@ -15,9 +15,37 @@ class GenUL(loader.Module):
     
     @loader.owner
     async def sglcmd(self, m: Message):
-      "<reply> - нужно ответить на сообщение с которого будет начинаться парсинг пользователей"
-      reply = await m.get_reply_message()
-      if not reply:
-        return await m.edit("бля")
-      else:
-        
+        "<reply> - нужно ответить на сообщение с которого будет начинаться парсинг пользователей"
+        max_users = 30 #default
+        symbols_add = [
+            '+',
+            'plus',
+            'плюс',
+            '➕',
+            '👍'
+        ]
+        if not m.chat:
+            return await m.edit("<b>Это не чат</b>")
+
+        usrlist = ''
+        if not reply:
+            return await m.edit("бля")
+        else:
+            c = 0
+            for msg in m.client.iter_messages(m.chat.id, offset_id = reply.id, reverse=True, limit = 400)
+                if max_users == c: break
+                c += 1
+                try:
+                    if msg.text[0:1] in symbols_add:
+                        user = utils.get_display_name(msg.sender)
+                        if msg.sender == None:
+                            user = msg.post_author
+                            #uid = 0
+                        else:
+                            uid = msg.sender.id
+                        if not user: user = m.chat.title
+                except TypeError: continue
+                except NameError: user = '* Аноним без должности'
+                userlist += '{}. {}\n'.format(c, user)
+        await message.edit(userlist)     
+                    
