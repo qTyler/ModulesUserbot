9ebdda853804c8eec7fea84f8c13d97c6e00ec03
qTyler <97607757+qTyler@users.noreply.github.com>
2022-12-15 09:29:54 +0300

    Update FreeDist.py

--- FreeDist.py
+++ FreeDist.py
@@ -1,7 +1,7 @@
 from .. import loader, utils
 from telethon.tl.types import Message
 from telethon.utils import get_display_name
-import datetime, requests
+import datetime, requests, random
 from time import strftime
 
 @loader.tds
@@ -10,7 +10,10 @@ class FreeDistribution(loader.Module):
 
     strings = {
         "name": "FreeDistribution",
+        "btnName": "🎁 Забрать халяву!",
+        "":"",
         "error_store_empty", "Склад пуст раздавать нечего. Пополните склад!",
+        
     }
   
     def __init__(self):
@@ -34,14 +37,51 @@ class FreeDistribution(loader.Module):
                 validator=loader.validators.Integer()
             ),         
             loader.ConfigValue( 
-                "Products",
+                "Store",
                 [ ],
                 doc=lambda: "Укажите ссылку на стафф (ключи, софт и т.д.), пример: https://privnote.com/QmPkpRR5#2NmQ0dOBG",
                 validator=loader.validators.Series(validator=loader.validators.Url())
             ),
         )
     
+    def _get_mark(self):
+        url = self.config['Store'][random.randint(0, len(self.config['Store']))]
+        return (
+            {
+                "text": self.config["btnName"],
+                "url": url,
+            }
+        )
+    
     @loader.unrestricted
+    async def infocmd(self, message: Message):
+        """Send userbot info"""
+
+        if self.config["custom_button"]:
+            await self.inline.form(
+                message=message,
+                text=self._render_info(True),
+                reply_markup=self._get_mark(),
+                **(
+                    {"photo": self.config["banner_url"]}
+                    if self.config["banner_url"]
+                    else {}
+                ),
+            )
+        else:
+            try:
+                await self._client.send_file(
+                    message.peer_id,
+                    self.config["banner_url"],
+                    caption=self._render_info(False),
+                )
+            except Exception:
+                await utils.answer(message, self._render_info(False))
+            else:
+                if message.out:
+                    await message.delete()
+    
+    @loader.owner
     async def fdstrbcmd(self, m: Message):
         """ <время проведение раздачи> <частота появление ссылки в час> <время жизни ссылки> 
         Запускает раздачу ништяков!
@@ -56,7 +96,7 @@ class FreeDistribution(loader.Module):
                 if len(args) == 3: self.config["LinkLifetime"]  = int(args[2])
             except ValueError: pass
             
-        if len(self.config['Products']) > 0:
+        if len(self.config['Store']) > 0:
           return await m.edit(self.strings("error_store_empty"))
         
         
